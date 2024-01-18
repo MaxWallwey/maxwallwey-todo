d545ea9d43119307993cb633353dc585956d2d32
Max Wallwey <max.wallwey@gmail.com>
2024-01-18 10:05:08 +0000

    Remove Comments

--- backend/cli/Todo.Cli/Program.cs
+++ backend/cli/Todo.Cli/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System.Globalization;
 using Todo.Cli.Menu;
 
-// Console.WriteLine(
-        // "What would you like to do?\n- To view all incompleted tasks, press 'i'\n- To view all completed tasks, press'c'\n- To add a new task, press 'a'\n- To mark a task as completed, press 'm'\n- To remove a task, press 'r'\n- To stop the application, press 'e'\n");
-
 var menu = new MenuNavigation();
 
 menu.Main();
