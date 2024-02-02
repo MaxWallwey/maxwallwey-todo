493499841674e05585cc2f626b331b56bc2e6324
Max Wallwey <max.wallwey@gmail.com>
2024-02-02 11:43:49 +0000

    Set Up Controller and In Memory Database

---
+++ backend/cli/Todo.API/Models/ToDoContext.cs
@@ -0,0 +1,12 @@
+namespace Todo.API.Models;
+
+using Microsoft.EntityFrameworkCore;
+public class ToDoContext : DbContext
+{
+    public ToDoContext(DbContextOptions<ToDoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ToDo?> Todos { get; set; } = null!;
+}
