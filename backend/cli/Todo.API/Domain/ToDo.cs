e31304ba1978d0696c1a9d7b04eb096e59fb314d
Max Wallwey <max.wallwey@gmail.com>
2024-02-05 11:26:43 +0000

    Updated Endpoints

--- backend/cli/Todo.API/Domain/ToDo.cs
+++ backend/cli/Todo.API/Domain/ToDo.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Todo.API.Models;
 
 public class ToDo
 {
-    protected internal ToDo(string name, bool isComplete = false)
+    protected internal ToDo(string? name, bool isComplete = false)
     {
         Id = Guid.NewGuid();
         Name = name;
@@ -11,6 +13,11 @@ public class ToDo
     }
 
     public Guid Id { get; set; }
+    
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(50, MinimumLength = 3,
+        ErrorMessage = "Name should be minimum 3 characters and a maximum of 50 characters")]
+    [DataType(DataType.Text)]
     public string Name { get; private set; }
     public DateTime CreatedAt { get; set; }
     public bool IsComplete { get; private set; }
