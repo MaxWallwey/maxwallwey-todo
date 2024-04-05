596b0bf41b9f3b6ab16e9f0d68e89bd4d62324ba
Max Wallwey <127151268+MaxWallwey@users.noreply.github.com>
2024-04-05 10:24:27 +0100

    Redesign portal and button (#18)

--- frontend/todo-portal/src/Components/Todo/Todo.tsx
+++ frontend/todo-portal/src/Components/Todo/Todo.tsx
@@ -1,34 +1,36 @@
-import {Buttons, TodoButton} from "../TodoButton";
+import { Buttons, TodoButton } from "../TodoButton";
 import React from "react";
 
 type TodoProp = {
-    id: string;
-    name: string;
-    isComplete: boolean;
-    removeTodo: () => void;
-    completeTodo: () => void;
-}
+  id: string;
+  name: string;
+  isComplete: boolean;
+  removeTodo: () => void;
+  completeTodo: () => void;
+};
 
 export const Todo: React.FC<TodoProp> = ({
-    id,
-    name,
-    isComplete,
-    removeTodo,
-    completeTodo,
+  id,
+  name,
+  isComplete,
+  removeTodo,
+  completeTodo,
 }) => (
-    <>
-        <div className="flex mb-4 items-center" id={id}>
-            <p className={`w-full ${isComplete ? "text-green" : ""}`}>
-                <span className={isComplete ? "bg-green/20" : ""}>{name}</span>
-            </p>
-
-            {!isComplete && (
-                <TodoButton variant={Buttons.complete} onClick={completeTodo} />
-            )}
-            
-            &nbsp;&nbsp;
-            
-            <TodoButton variant={Buttons.remove} onClick={removeTodo} />
-        </div>
-    </>
+  <>
+    <div className="flex mb-4 items-center" id={id}>
+      <p className={`w-full`}>
+        <span className={isComplete ? "font-style: italic" : ""}>{name}</span>
+      </p>
+      {!isComplete && (
+        <TodoButton variant={Buttons.complete} onClick={completeTodo} />
+      )}
+      {isComplete && (
+        <button className="flex-no-shrink py-1 px-2 border-2 rounded text-xl  bg-green border-green hover:brightness-110">
+          ✔️
+        </button>
+      )}
+      &nbsp;&nbsp;
+      <TodoButton variant={Buttons.remove} onClick={removeTodo} />
+    </div>
+  </>
 );
